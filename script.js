ef2f28289780f19f1770261a55a4d07730980014
Colton Firestone <colton.firestone@gmail.com>
2023-07-08 00:15:36 -0700

    completely reworked the js to make it scalable in more ways

--- script.js
+++ script.js
@@ -42,70 +42,86 @@ const quizOneQuestions = [
 
 
 // start Quiz 2 question Bank Object
-const quizTwoQuestions = [
-    {
-        question: "q2 1+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
+ const quizTwoQuestions = [
+     {
+         question: "q2 1+2 = ?",
+         options: ["I1", "I2", "I3", "C"],
         answer: "C"
-    },
-    {
-        question: "q2 2+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
-        answer: "C"
-    },
-    {
-        question: "q2 3+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
-        answer: "C"
-    },
-    {
-        question: "q2 4+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
-        answer: "C"
-    }
-]
-// end quiz 2 question bank Object
+     },
+     {
+         question: "q2 2+2 = ?",
+         options: ["I1", "I2", "I3", "C"],
+         answer: "C"
+     },
+     {
+         question: "q2 3+2 = ?",
+         options: ["I1", "I2", "I3", "C"],
+         answer: "C"
+     },
+     {
+         question: "q2 4+2 = ?",
+         options: ["I1", "I2", "I3", "C"],
+         answer: "C"
+     }
+ ]
+ // end quiz 2 question bank Object
 
-let questionNumber = 0
+ 
 
-// function that runs the selected quiz
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let pageQuestion = document.querySelector('.question');
+let pageOptionSection = document.querySelector('.optionSection');
+let submitButton = document.querySelector('.optionSection');
+
+
+;// function that runs the selected quiz
 function runQuiz(quiz) {
-    //randomizes the questions
-    let shuffledList = randomize(quiz);
-    
-    //determines the length of the quiz based on the amount of questions
-    for (i=0; i < shuffledList.length; i ++){
-    
-    // asks the questions in the random order determined by first function
-    const orderedQuestion = shuffledList[questionNumber];
-    questionNumber += 1;
-
-    //randomizes the options
-    
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
-  //this do while loop runs as long as a certain condition is happening
-  //in this case it is when the user wants to retry the question
- do{
-  //temporarly what asks the questions
-    randomize(orderedQuestion.options);
-    var userAnswer = prompt(orderedQuestion.question)
-
-    //temporarly checks to see if the answer is correct
-    if (userAnswer === orderedQuestion.answer){
-        console.log("woohoo")
-    
-    }else{
-//temporary retry element with lower penalty
-var tryAgain = confirm("try again? -5 seconds")
-//temporary skip element with higher penalty
-var skip = confirm("skip? -10 seconds")
-if (tryAgain === true ){
-    console.log("-5")
-}}
-} while (tryAgain===true)
-}}
-
+    let shuffledQuestionOrder = randomize(quiz);
+    let questionNumber = 0;
   
+    function displayQuestion() {
+      let currentQuestion = shuffledQuestionOrder[questionNumber];
+      let shuffledOptions = randomize(currentQuestion.options);
+      console.log(shuffledOptions);
+      pageQuestion.innerHTML = currentQuestion.question;
+  
+      pageOptionSection.innerHTML = ''; // Clear the options section before adding new options
+  
+      for (let i = 0; i < currentQuestion.options.length; i++) {
+        let optionValue = shuffledOptions[i];
+        let htmlOptionToAdd = `<button class="option"><p>&#8226; ${optionValue}</p></button>`;
+        pageOptionSection.innerHTML += htmlOptionToAdd;
+      }
+  
+      // Update the pageOptions variable after adding new options
+      let pageOptions = document.querySelectorAll('.option');
+  
+      pageOptions.forEach((option, index) => {
+        option.addEventListener('click', () => {
+          handleOptionClick(index);
+        });
+      });
+    }
+  
+    function handleOptionClick(optionIndex) {
+      // Handle the logic for the selected option
+      console.log(`Option ${optionIndex + 1} selected`);
+    }
+  
+    displayQuestion();
+  }
+  
+  // ...
+  
+  runQuiz(quizOneQuestions);
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// countdown(quiz.length * 1)  
 // do {
 //       userAnswer = prompt(orderedQuestion.question);
 
@@ -124,10 +140,27 @@ function randomize(list) {
       const j = Math.floor(Math.random() * (i + 1));
       [list[i], list[j]] = [list[j], list[i]];
     }
-    return list;
-    let shuffledList = randomize(list);
+    return list;  
   }
   
+//basic quiz countdown structure finished
+  function countdown(total) {
+    var timeLeft = total;
+  
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      timeLeft--;
+      
+      if (timeLeft === 0) {
+        clearInterval(timeInterval);
+        console.log("finished")
+      }
+    }, 1000);
+  }
+
+
 //temporarly selects and starts the quiz
-//   runQuiz(quizOneQuestions)
-  runQuiz(quizTwoQuestions)
+
+
+// runQuiz(quizTwoQuestions)
+  
