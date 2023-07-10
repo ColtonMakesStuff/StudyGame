3cf0c8c9af43f1844b167e2a4a1c3bd37ba0090e
Colton Firestone <colton.firestone@gmail.com>
2023-07-10 00:07:30 -0700

    got most things working

--- script.js
+++ script.js
@@ -14,28 +14,163 @@
 //  }
 
 
+  
+const quizSampleQuestions = [
+    {
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {
+        question:   "test \n\n<ul> \n<p> X </p>\n<p> X </p> \n<p> X </p> \n</ul>",
+    
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+]
+
 //start quiz one question bank object
 const quizOneQuestions = [
-    {
-        question: "1+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
-        answer: "C"
+    { //1
+        question: "What is the output of the following code?\n\n<ul>\n<p>var x = 10;</p>\n<p>var y = x * 2;</p>\n<p>console.log(y);</p>\n</ul>",
+        options: ["20", "10", "5", "NaN"],
+        answer: "20"
     },
-    {
-        question: "2+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
-        answer: "C"
+    { //2
+        question: "Which of the following is not a valid JavaScript variable name?",
+        options: ["my_variable", "_myVariable", "$myVariable", "my-variable"],
+        answer: "$myVariable"
     },
-    {
-        question: "3+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
-        answer: "C"
+    {  //3
+        question: "What is the output of the following code?\n\n<ul>\n<p>var x = true;</p>\n<p>var y = false;</p>\n<p>console.log(x && y);</p>\n</ul>",
+        options: ["true", "false", "Nan", "undefined"],
+        answer: "false"
     },
-    {
-        question: "4+2 = ?",
-        options: ["I1", "I2", "I3", "C"],
-        answer: "C"
-    }
+    {//4
+        question: "Which of the following is not a valid JavaScript operator?",
+        options: ["+", "-", "*", "/", "?"],
+        answer: "?"
+    },
+    {//5
+        question: "What is the output of the following code?\n\n<ul>\n<p>var x = 10;</p>\n<p>var y = 20;</p>\n<p>if (x > y) {</p>\n<p>console.log('x is greater than y');</p>\n<p>} else {</p>\n<p>console.log('y is greater than x')};</p>\n</ul>",
+        options: ["x is greater than y", "y is greater than x", "undefined"],
+       answer: "y is greater than x"
+    },
+    {//6
+        question: "Which of the following is not a valid JavaScript function?",
+        options: [" function myFunction() { // code here }", "function myFunction(x, y) { // code here }", "function myFunction() { return x + y; }", " function myFunction() { var x = 10; var y = 20; return x + y; }"],
+       answer: "function myFunction() { var x = 10; var y = 20; return x + y; }"
+    },
+    {//7
+        question: "What is the output of the following code?\n\n<ul>\n<p>var x = [1, 2, 3];</p>\n<p>console.log(x[0]);</p>\n</ul>",
+        options: [
+                  "1"
+                , "2"
+                , "3"
+                , "undefined"
+                ],
+       answer: "1"
+    },
+    {//8
+        question: "What is the output of the following code?\n\n<ul> \n<p>var x = {</p>\n<p>name: 'John Doe',</p> \n<p>age: 30,};</p> \n<p>console.log(x.name);</p> \n</ul>",
+        options: [
+                  "John Doe"
+                , "30"
+                , "undefined"
+                , "NaN"
+                ],
+       answer: "John Doe"
+    },
+    {//9
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {//10
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {//11
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {//12
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {//13
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {//14
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {//15
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
+    {//16
+        question: "q2 1+2 = ?",
+        options: [
+                  "I1"
+                , "I2"
+                , "I3"
+                , "C"
+                ],
+       answer: "C"
+    },
 ]
 //end quiz one question bank object
 
@@ -109,23 +244,94 @@ let timeScore = document.querySelector('.timeScore')
 let timerEl = document.querySelector('.timer')
 let playAgain = document.querySelector('.playAgain')
 
+//quiz list navigation
+let quizOne = document.querySelector('#quizOne');
+let quizTwo = document.querySelector('#quizTwo');
+let quizThree = document.querySelector('#quizThree');
+let quizList = document.querySelector('.quizList');
+let daRulez = document.querySelector('.daRulez');
 
+quizOne.addEventListener("click", function() {
+   startTheQuiz(quizOneQuestions);
+  });
+quizTwo.addEventListener("click", function() {
+    startTheQuiz(quizTwoQuestions);
+  });
+quizThree.addEventListener("click", function() {
+    alert("error")
+  });
+
+// start visual functions
+ let startButton = document.querySelector('.start');
+ let gameSection = document.querySelector('.game');
+ let gameOverScreen = document.querySelector('.finished')
+
+function startVisualQuiz() {
+    
+    
+    quizList.classList.add("hide")
+    daRulez.classList.add("hide")
+ }
+
+ 
+ function startTheQuiz(quiz) {
+    startVisualQuiz()
+    startCountdown(quiz)
+    
+ };
+
+
+ function startCountdown(quiz) {
+    var timeLeft = 6;
+  
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+       timeLeft--; 
+       startButton.classList.remove("hide");
+      startButton.textContent = "quiz starts in "+timeLeft +"!";
+      
+      if (timeLeft === 0) {
+        clearInterval(timeInterval);
+        runQuiz(quiz);  
+        startButton.classList.add("hide");
+        gameSection.classList.remove("game"); 
+       
+      }
+    }, 1000);
+  }
+//end visual functions
+
+
+// this starts the quiz
+startButton.addEventListener("click", function() {
+    startTheQuiz(quizTwoQuestions);
+  });
+
+
+//temporarly selects and starts the quiz
 
 let pageQuestion = document.querySelector('.question');
 let pageOptionSection = document.querySelector('.optionSection');
 let submitButton = document.querySelector('.optionSection');
+let questionCounter = document.querySelector('.questionCounter');
+let correctCounter = document.querySelector('.correctCounter');
 
 
 ;// function that runs the selected quiz
 function runQuiz(quiz) {
+    
+
     answeredNumber = 0
     correctNumber = 0
     percentCorrect.innerHTML = 0
+    correctCounter.innerHTML = `<p>${correctNumber}/${answeredNumber} correct!</p>`
+    questionCounter.innerHTML = `<p>${quiz.length-answeredNumber} left</p>`
 //starts timer for the quiz
     countDown(quiz.length*5)
 //shuffles the order of the questions in the quiz
     let shuffledQuestionOrder = randomize(quiz);
 
+    
 //establishes base question number
     let questionNumber = 0;
     let currentQuestion = shuffledQuestionOrder[questionNumber];
@@ -171,6 +377,8 @@ function runQuiz(quiz) {
             console.log("darn")
         }
         answeredNumber ++;
+        correctCounter.innerHTML = `<p>${correctNumber}/${answeredNumber} correct!</p>`
+        questionCounter.innerHTML = `<p>${quiz.length-answeredNumber} left</p>`
         //little bit of math for getting the final score
         let percentage  =(correctNumber / answeredNumber) * 100 + "% accuracy !";
         percentCorrect.innerHTML = percentage
@@ -188,7 +396,7 @@ function runQuiz(quiz) {
        
     } else {
         //ends the game if all questions have been answered
-        gameOver()
+        gameOver(quiz)
     }
 }
 //this is what gets this all going
@@ -222,6 +430,7 @@ function countDown(timeAllotted) {
     timeScore.innerHTML = `<p> you finished in ${timeTaken} seconds! </p>`;
     if (timeLeft === 0) {
       clearInterval(timeInterval);
+      timeScore.innerHTML = `<p>oh no! you didn't finish in time :( please try again`
       gameOver();
     }
     console.log("Time left:", timeLeft);
@@ -229,43 +438,18 @@ function countDown(timeAllotted) {
     
   }, 1000);
 }
-function gameOver(){
+function gameOver(quiz){
     clearInterval(timeInterval)
     gameOverScreen.classList.remove("finished");
     gameSection.classList.add("game");
     gameOverScreen.classList.remove("hide")
+    playAgain.addEventListener("click", function() {
+        startTheQuiz(quiz);
+        gameOverScreen.classList.add("hide")
+      });
  } 
 
 
-// start visual functions
- let startButton = document.querySelector('.start');
- let gameSection = document.querySelector('.game');
- let gameOverScreen = document.querySelector('.finished')
-
-function startVisualQuiz() {
-    startButton.classList.add("hide");
-    gameSection.classList.remove("game");
- }
-
-
- 
- function startTheQuiz(quiz) {
-    startVisualQuiz()
-    runQuiz(quiz)
- }
-//end visual functions
-
-
-// this starts the quiz
-startButton.addEventListener("click", function() {
-    startTheQuiz(quizTwoQuestions);
-  });
-
-  playAgain.addEventListener("click", function() {
-    startTheQuiz(quizTwoQuestions);
-    gameOverScreen.classList.add("hide")
-  });
-//temporarly selects and starts the quiz
 
 
 // runQuiz(quizTwoQuestions)
